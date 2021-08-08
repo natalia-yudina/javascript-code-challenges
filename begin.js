287a9970066ddc3f628933decab704669d1f8d2b
Natalia Iudina <78476252+natalia-yudina@users.noreply.github.com>
2021-08-08 22:12:16 +1200

    Add example

--- begin.js
+++ begin.js
@@ -1,4 +1,4 @@
-
+// name of a class starts with an uppercase letter
 class Movie {
   constructor(title, director, genre, releaseYear, rating) {
     // assign the values to the object instance using "this" keyword
@@ -8,8 +8,16 @@ class Movie {
     this.releaseYear = releaseYear;
     this.rating = rating;
   }
+    // method
     getOverview(){
-      console.log(this.title + ", a" + this.genre + " film directed by " + this.director + " was released in " + this.releaseYear + ". It received a rating of " + this.rating);
+      return (this.title + ", a " + this.genre + " film directed by " + this.director + " was released in " + this.releaseYear + ". It received a rating of " + this.rating);
     }
 
 }
+
+// Example
+// const Cruella = new Movie("Cruella", "Craig Gillespie", "Comedy/Crime", 2021, 4.7);
+// const LaLaLand = new Movie("La La Land", "Damien Chazelle", "Musical/Romance", 2016, 4.5);
+
+// console.log(Cruella.getOverview());
+// console.log(LaLaLand.getOverview());
