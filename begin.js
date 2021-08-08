9a169183b05f562b372f43a25e882377f1d5da90
Natalia Iudina <78476252+natalia-yudina@users.noreply.github.com>
2021-08-08 18:24:04 +1200

    Update begin.js

--- begin.js
+++ begin.js
@@ -33,3 +33,11 @@ class Book {
   }
 
 }
+
+// Example
+// const GoodToGreat = new Book("Good To Great", "Jim Collins", "662099", 4);
+// console.log(GoodToGreat.availability);
+// GoodToGreat.restock(10);
+// console.log(GoodToGreat.availability);
+// GoodToGreat.sell(14);
+// console.log(GoodToGreat.availability);
