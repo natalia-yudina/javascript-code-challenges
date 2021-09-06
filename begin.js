6b6575c8468e4485b9e04a55427c778c8ea3e36a
Natalia Iudina <78476252+natalia-yudina@users.noreply.github.com>
2021-09-06 13:50:26 +1200

    Add files via upload

---
+++ begin.js
@@ -0,0 +1,11 @@
+const username = Symbol("s_username");
+const password = Symbol("s_password");
+
+const user = {
+  [username]: "natalia",
+  [password]: "1234567",
+  age: 27,
+};
+
+// console.log(user.username);
+// console.log(user.age);
